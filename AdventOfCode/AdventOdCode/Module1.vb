bd778a0e3ff2415366158c7d02d2f826860073fc
Jason Flower <35689497+JJ-Styles@users.noreply.github.com>
2019-11-03 20:16:34 +0000

    Project upload

---
+++ AdventOfCode/AdventOdCode/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+Module Module1
+    Sub Main()
+        Dim file As String
+        Dim Command() As String
+        Dim Direction As Integer
+        Dim distance As Integer
+        Dim FileReader As StreamReader = New StreamReader("AdventOfCode1a.txt")
+        file = FileReader.ReadToEnd
+        Command = file.Split(",")
+        For i = 0 To (Command.Length - 1)
+            If Mid(Command(i), 1, 1) = "R" Then
+                Direction += 90
+                distance += Mid(Command(i), 2, (Command(i).Length - 1))
+            Else
+                Direction -= 90
+                distance -= Mid(Command(i), 2, (Command(i).Length - 1))
+            End If
+        Next
+        If Direction > 360 Or Direction < 0 Then
+            Direction -= 360
+        End If
+        Console.WriteLine(Direction)
+        Console.WriteLine(distance)
+        Console.ReadKey()
+    End Sub
+
+End Module
